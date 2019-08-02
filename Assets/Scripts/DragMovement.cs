9b0cd1e9c447ab5b430171f54517808549b1e9e9
VR <josh.piotr@gmail.com>
2019-08-02 16:48:29 +1000

    fixed drag movement and theres a bunch of other files i guess

--- Assets/Scripts/DragMovement.cs
+++ Assets/Scripts/DragMovement.cs
@@ -6,22 +6,35 @@ public class DragMovement : MonoBehaviour
 {
     IInput m_input;
 
-    Transform m_dragPoint;
-    Vector2 m_prevPoint;        //last frames point
+    public Transform m_dragPoint;
+    Vector3 m_prevPoint;        //last frames point
+    public float m_sensitivity = 30;
 
     void Start()
     {
         m_input = GetComponent<IInput>();
+
     }
 
     void Update()
     {
         if(m_input.dragMovement)
         {
-            Vector3 newPos = new Vector3( m_prevPoint.x - m_dragPoint.position.x, m_dragPoint.position.y, m_prevPoint.y - m_dragPoint.position.z );
+            //Vector3 newPos = new Vector3( m_prevPoint.x - m_dragPoint.position.x, m_dragPoint.position.y, m_prevPoint.y - m_dragPoint.position.z );
+            //transform.position = newPos;
+            Vector3 pointDiff = m_dragPoint.localPosition - m_prevPoint;
 
-            transform.position = newPos;
+            pointDiff.y = 0;
+
+            transform.position -= pointDiff * m_sensitivity;
+            //transform.position = m_dragPoint.localPosition;
+
+            Debug.Log(m_dragPoint.localPosition);
+            //Debug.Log(m_dragPoint.position);
+
+            //Debug.Log(pointDiff * 1000);
         }
-        m_prevPoint = m_dragPoint.position;
+
+        m_prevPoint = m_dragPoint.localPosition;
     }
 }
