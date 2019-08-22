01ea63053e5c86f80816249bcc4545dc38b56811
JoshMosk <josh.piotr@gmail.com>
2019-08-22 17:00:17 +1000

    started work on the additive scene loading

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,38 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController Instance { set; get; }
-
-    CurrentLevel m_currentLevel;
-
-    public void Load(string sceneName)
-    {
-        if(!SceneManager.GetSceneByName(sceneName).isLoaded)
-        {
-            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-        }
-    }
-
-    public void Unload(string sceneName)
-    {
-        if(SceneManager.GetSceneByName(sceneName).isLoaded)
-        {
-            SceneManager.UnloadSceneAsync(sceneName);
-        }
-    }
-
     
+    public List<SceneDep> sceneDeps;
 
-    enum CurrentLevel
+    public SceneAsset startScene;
+
+
+    //public void Load(string sceneName)
+    //{
+    //    if(!SceneManager.GetSceneByName(sceneName).isLoaded)
+    //    {
+    //        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    //    }
+    //}
+
+    //public void Unload(string sceneName)
+    //{
+    //    if(SceneManager.GetSceneByName(sceneName).isLoaded)
+    //    {
+    //        SceneManager.UnloadSceneAsync(sceneName);
+    //    }
+    //}
+
+    private void Start()
     {
-        level1 = 0,
-        level2,
-        level3,
-        level4,
-        level5,
-        epilogue
+        //SceneManager.LoadSceneAsync(SceneManager.GetSceneByName("test1").buildIndex, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(startScene.name, LoadSceneMode.Additive);
     }
+
+    public void LoadDepFromTargetScene(SceneAsset s)
+    {
+        SceneDep temp = sceneDeps.Find((SceneDep a) => { return a.currentScene == s; });
+
+        foreach (Scene x in SceneManager.GetAllScenes())//for every scene currely loaded
+        {
+            //if scene loaded isnt current or dep get it out //currently the thing is yes when it meant be no
+            if (x.name != temp.currentScene.name )
+            {
+                if( temp.dependantScenes.Find((SceneAsset a) => { return a.name == x.name; }) == null)
+                SceneManager.UnloadSceneAsync(x.name);
+            }
+        }
+        //go through scenedep and make sure all scenes are loaded
+        foreach(SceneAsset x in temp.dependantScenes)
+        {
+            Scene y = SceneManager.GetSceneByName(x.name);
+            if(SceneManager.GetSceneByName(x.name) == null)
+            {
+                SceneManager.LoadSceneAsync(x.name, LoadSceneMode.Additive);
+            }
+        }
+
+    }
+    
 }
+
+[System.Serializable]
+public class SceneDep
+{
+    //have the scenes that this scene is dependant on
+    public SceneAsset currentScene;
+    public List<SceneAsset> dependantScenes;
+
+}
+
---
+++ Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public SceneAsset currentScene;
+    public SceneController control;
+
+    public void Start()
+    {
+        control = GameObject.Find("SceneController").GetComponent<SceneController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            Debug.Log(currentScene.name + " has loaded");
+        if(other.tag == "Player")
+        {
+            //SceneController.Instance.LoadDepFromTargetScene(currentScene);
+            control.LoadDepFromTargetScene(currentScene);
+        }
+    }
+}
