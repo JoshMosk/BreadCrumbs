0841edaee6c964de99a7e809db7fecbca7115f00
JoshMosk <josh.piotr@gmail.com>
2019-09-13 17:01:08 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/HandAnimController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimController : MonoBehaviour
+{
+	public Animator m_animator;
+	public IInput m_input;
+
+	private void Update()
+	{
+		m_animator.se
+	}
+}
---
+++ Assets/Scripts/Puzzle3.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3 : MonoBehaviour
+{
+	CorruptPlayer m_corruptPlayer;
+
+	GameObject m_gate;
+
+
+	void Start()
+    {
+		m_corruptPlayer = FindObjectOfType<CorruptPlayer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (m_corruptPlayer.m_isCorrupt)
+		{
+			if (other.gameObject.name == "Lylah")
+			{
+				//need to show shadow only in light mode// DONE
+				//once triggered make sure inly lylah// DONE
+				//disable trigger
+				//allow the wall collider to be disabled while on dark
+
+				//you know what im just gonna write swhit, so whebn she walks in light then we swap the texture, also have two meshes that 
+				//allows for lylah to walk between the building that means have a coll that is disable when stand in light
+			}
+		}
+	}
+}
