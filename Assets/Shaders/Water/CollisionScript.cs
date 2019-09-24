d45ace9b7e3889799e62afbadc439ee0132f7e76
Cameron <Cameronmcg83@gmail.com>
2019-09-25 09:59:55 +1000

    shader water and laser pointer  and texture lazer line

---
+++ Assets/Shaders/Water/CollisionScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    private int waveNumber;
+    public float distanceX, distanceZ;
+    public float[] rippleAmplitude;
+    public float magnitudeDivider;
+    Renderer rend;
+
+    Mesh Mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh = GetComponent<MeshFilter>().mesh;
+        rend = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        for (int i = 0; i < 8; i++)
+        {
+
+            rippleAmplitude[i] = rend.material.GetFloat("_RippleAmplitude" + (i + 1));
+            if (rippleAmplitude[i] > 0)
+
+            {
+                rend.material.SetFloat("RippleAmplitude" + (i + 1), rippleAmplitude[i] * 0.98f);  // over time ripple ruduces by 2% (0.98)
+            }
+            if (rippleAmplitude[i] < 0.05)
+            {
+                rend.material.SetFloat("RippleAmplitude" + (i + 1), 0);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.rigidbody)
+        {
+            waveNumber++;
+            if (waveNumber == 9)
+            {
+                waveNumber = 1;
+            }
+            rippleAmplitude[waveNumber - 1] = 0;
+
+            distanceX = this.transform.position.x - col.gameObject.transform.position.x;
+            distanceZ = this.transform.position.z - col.gameObject.transform.position.z;
+
+            rend.material.SetFloat("_RippleOffsetX" + waveNumber, distanceX / Mesh.bounds.size.x * 2.5f);
+            rend.material.SetFloat("_RippleOffsetZ" + waveNumber, distanceZ / Mesh.bounds.size.z * 2.5f);
+
+            rend.material.SetFloat("_RippleAmplitude" + waveNumber, col.rigidbody.velocity.magnitude * magnitudeDivider);
+        }
+
+    }
+}
