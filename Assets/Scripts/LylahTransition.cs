2a9716a36eef76f7ab262934af7c0836a045ee83
JoshMosk <josh.piotr@gmail.com>
2019-09-26 14:59:05 +1000

    added Lylah transition script, just needs to be tested
    also added in primitive new puzzle 3 so yeah?

---
+++ Assets/Scripts/LylahTransition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LylahTransition : MonoBehaviour
+{
+	Renderer rend;
+	CorruptPlayer corrupt;
+
+	public float m_transitionTime = 1.0f;
+	float progress;		//corrupt == 1
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		corrupt = FindObjectOfType<CorruptPlayer>();
+		rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(corrupt.m_isCorrupt)
+		{
+			progress += Time.deltaTime / m_transitionTime;
+		}
+		else
+		{
+			progress -= Time.deltaTime / m_transitionTime;
+		}
+
+		if(progress < 0)
+		{
+			progress = 0;
+		}
+		else if(progress > 1)
+		{
+			progress = 1;
+		}
+
+		rend.material.SetFloat("_DissolveSlider", progress);
+    }
+}
--- Assets/Scripts/Puzzle3.cs
+++ Assets/Scripts/Puzzle3.cs
@@ -4,57 +4,86 @@ using UnityEngine;
 
 public class Puzzle3 : MonoBehaviour
 {
-	CorruptPlayer m_corruptPlayer;		//the player
+	//TODO
+	//new system
+	//when player is in the light while in corrupt unlocks puzzle
+	//puzzle triggers lamp post to turn green and the lamp will tell puzzle master that puzzle 3 is complete
 
-	public GameObject m_gate;      //used to block lylah
-	public GameObject m_holeInTheWall;          //the hole that will disppear once puzzle is solved
-	public Material m_buildingSolvedMat;
+	public CorruptPlayer corrupt;
+	public GameObject lamp;
 
-	[SerializeField]
-	bool m_puzzleComplete = false;
+	private void Start()
+	{
+		corrupt = FindObjectOfType<CorruptPlayer>();
+	}
 
-	void Start()
-    {
-		m_corruptPlayer = FindObjectOfType<CorruptPlayer>();
-    }
-
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (m_corruptPlayer.m_isCorrupt == false)
+		if(corrupt.m_isCorrupt)
 		{
-			if (other.gameObject.name == "Lylah")
+			if (other.name == "Lylah")
 			{
-				if(m_puzzleComplete == false)
-				{
-					m_holeInTheWall.GetComponent<Renderer>().material = m_buildingSolvedMat;
-
-					//make building disappear when in dark
-					m_holeInTheWall.AddComponent<MajorVRProj.CorruptBuilding>();
-					FindObjectOfType<MajorVRProj.CorruptBuildingController>().corruptBuildings.Add(m_holeInTheWall.GetComponent<MajorVRProj.CorruptBuilding>());
-
-					//disable gate
-					m_gate.GetComponent<Collider>().enabled = false;
-
-					//puzzle is complete
-					m_puzzleComplete = true;
-				}
-
-
-
-
-				//need to show shadow only in light mode// DONE
-				//once triggered make sure inly lylah// DONE
-				//disable trigger
-				//allow the wall collider to be disabled while on dark
-
-				//you know what im just gonna write swhit, so whebn she walks in light then we swap the texture, also have two meshes that 
-				//allows for lylah to walk between the building that means have a coll that is disable when stand in light
+				lamp.GetComponent<Light>().color = Color.green;
 			}
 		}
 	}
+
+	//CorruptPlayer m_corruptPlayer;		//the player
+
+	//public GameObject m_gate;      //used to block lylah
+	//public GameObject m_holeInTheWall;          //the hole that will disppear once puzzle is solved
+	//public Material m_buildingSolvedMat;
+
+	//[SerializeField]
+	//bool m_puzzleComplete = false;
+
+	//void Start()
+	//   {
+	//	m_corruptPlayer = FindObjectOfType<CorruptPlayer>();
+	//   }
+
+	//   void Update()
+	//   {
+
+	//   }
+
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	if (m_corruptPlayer.m_isCorrupt == false)
+	//	{
+	//		if (other.gameObject.name == "Lylah")
+	//		{
+	//			if(m_puzzleComplete == false)
+	//			{
+	//				m_holeInTheWall.GetComponent<Renderer>().material = m_buildingSolvedMat;
+
+	//				//make building disappear when in dark
+	//				m_holeInTheWall.AddComponent<MajorVRProj.CorruptBuilding>();
+	//				FindObjectOfType<MajorVRProj.CorruptBuildingController>().corruptBuildings.Add(m_holeInTheWall.GetComponent<MajorVRProj.CorruptBuilding>());
+
+	//				//disable gate
+	//				m_gate.GetComponent<Collider>().enabled = false;
+
+	//				//puzzle is complete
+	//				m_puzzleComplete = true;
+	//			}
+
+
+
+
+	//			//need to show shadow only in light mode// DONE
+	//			//once triggered make sure inly lylah// DONE
+	//			//disable trigger
+	//			//allow the wall collider to be disabled while on dark
+
+	//			//you know what im just gonna write swhit, so whebn she walks in light then we swap the texture, also have two meshes that 
+	//			//allows for lylah to walk between the building that means have a coll that is disable when stand in light
+	//		}
+	//	}
+	//}
 }
