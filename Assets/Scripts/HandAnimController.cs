74f1bf72d8030bcc332b76b15ecaff991fcdc135
JoshMosk <josh.piotr@gmail.com>
2019-09-26 10:41:37 +1000

    added graffiti swap and pointy anim, now test it max

--- Assets/Scripts/HandAnimController.cs
+++ Assets/Scripts/HandAnimController.cs
@@ -7,8 +7,32 @@ public class HandAnimController : MonoBehaviour
 	public Animator m_animator;
 	public IInput m_input;
 
+	public float transitionTime = 0.3f;
+
+	float pointProgress;
+
 	private void Update()
 	{
-	//	m_animator.se
+		//m_animator.SetTrigger();
+
+		if(m_input.pointAndClick)
+		{
+			pointProgress += Time.deltaTime / transitionTime;
+		}
+		else
+		{
+			pointProgress -= Time.deltaTime / transitionTime;
+		}
+
+		if(pointProgress < 0)
+		{
+			pointProgress = 0;
+		}
+		else if(pointProgress > 1)
+		{
+			pointProgress = 1;
+		}
+
+		m_animator.SetFloat("pointFloat", pointProgress);
 	}
 }
