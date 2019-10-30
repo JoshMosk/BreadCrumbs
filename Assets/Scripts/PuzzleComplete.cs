c59c50461041edfb26fea7499c44e75c5be0270a
VR <josh.piotr@gmail.com>
2019-10-30 17:06:49 +1100

    made puzzle complete nicer, fixed some input, line rend is still fucked

--- Assets/Scripts/PuzzleComplete.cs
+++ Assets/Scripts/PuzzleComplete.cs
@@ -11,15 +11,29 @@ public class PuzzleComplete : MonoBehaviour
 
 	public Light m_light;
 
+    public float m_transitionTime = 0.5f;
+    float m_progress = 0;
+
+
     void Start()
     {
 		m_light.color = m_puzzleIncompleteColour;
     }
 
-	public void CompletePuzzle()
+    private void Update()
+    {
+        if(m_puzzleComplete == true)
+        {
+            m_progress += Time.deltaTime / m_transitionTime;
+
+            m_light.color = Color.Lerp(m_puzzleIncompleteColour, m_puzzleCompleteColour, m_progress);
+        }
+    }
+
+    public void CompletePuzzle()
 	{
 		m_puzzleComplete = true;
 
-		m_light.color = m_puzzleCompleteColour;
+		//m_light.color = m_puzzleCompleteColour;
 	}
 }
