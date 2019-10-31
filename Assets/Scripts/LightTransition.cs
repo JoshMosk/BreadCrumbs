4d60cf2474093b65913615507e6440d7075b54dc
VR <josh.piotr@gmail.com>
2019-11-01 10:39:03 +1100

    ignored occlusion culling, fixed light transition

--- Assets/Scripts/LightTransition.cs
+++ Assets/Scripts/LightTransition.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class LightTransition : MonoBehaviour
 {
+    public Color m_uncorruptCol;
+    public Color m_corruptCol;
 
-	public float m_unCorruptIntensity;
+    public float m_uncorruptIntensity;
 	public float m_corruptIntensity;
 
 	public float m_transitionTime;
+
+
 	float m_progress;       //progress is 0 at  full uncorrupt, 1 at full corrupt
-	float m_unprogress = 1;
 
 	public bool m_isCorrupt;
 
@@ -29,31 +32,25 @@ public class LightTransition : MonoBehaviour
 
 	public void StartTransition()
 	{
-
 		if(m_isCorrupt)
 		{
 			m_progress += Time.deltaTime / m_transitionTime;
-			m_unprogress -= Time.deltaTime / m_transitionTime;
-
-			m_light.intensity = Mathf.Lerp(m_light.intensity, m_corruptIntensity, m_progress);
 		}
 		else
 		{
-			m_unprogress += Time.deltaTime / m_transitionTime;
 			m_progress -= Time.deltaTime / m_transitionTime;
-
-			m_light.intensity = Mathf.Lerp(m_light.intensity, m_unCorruptIntensity, m_unprogress);
 		}
 
-		if(m_progress > 1)
+        if (m_progress > 1)
 		{
 			m_progress = 1;
-			m_unprogress = 0;
 		}
 		else if(m_progress < 0)
 		{
 			m_progress = 0;
-			m_unprogress = 1;
 		}
-	}
+
+        m_light.color = Color.Lerp(m_uncorruptCol, m_corruptCol, m_progress);
+        m_light.intensity = Mathf.Lerp(m_light.intensity, m_corruptIntensity, m_progress);
+    }
 }
