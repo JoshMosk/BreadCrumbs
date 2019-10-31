5ec2e9f320534a6dd1b987ea444f4af201b2989e
VR <josh.piotr@gmail.com>
2019-10-31 12:18:45 +1100

    made bounds, max and vic you need to set it

---
+++ Assets/Scripts/DebugBounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DebugBounds : MonoBehaviour
+{
+    DragMovement drag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drag = GetComponent<DragMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(new Vector3(drag.m_minBounds.x, 10, drag.m_maxBounds.y), new Vector3(drag.m_maxBounds.x, 10, drag.m_maxBounds.y));//line from top left to top right
+        Debug.DrawLine(new Vector3(drag.m_maxBounds.x, 10, drag.m_maxBounds.y), new Vector3(drag.m_maxBounds.x, 10, drag.m_minBounds.y));//line from top left to bottom right
+        Debug.DrawLine(new Vector3(drag.m_maxBounds.x, 10, drag.m_minBounds.y), new Vector3(drag.m_minBounds.x, 10, drag.m_minBounds.y));//line from bottom right to bottom left
+        Debug.DrawLine(new Vector3(drag.m_minBounds.x, 10, drag.m_minBounds.y), new Vector3(drag.m_minBounds.x, 10, drag.m_maxBounds.y));//line from bottom left to top left
+    }
+}
--- Assets/Scripts/DragMovement.cs
+++ Assets/Scripts/DragMovement.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(DebugBounds))]
 public class DragMovement : MonoBehaviour
 {
     IInput m_input;
 
     public Transform m_dragPoint;
-    Vector3 m_prevPoint;        //last frames point
+    public Vector3 m_prevPoint;
     public float m_sensitivity = 30;
+
+    public Vector2 m_maxBounds = new Vector2(100, 100);
+    public Vector2 m_minBounds = new Vector2(-100, -100);
+    Vector3 m_prevPos;
 
     void Start()
     {
@@ -18,23 +23,37 @@ public class DragMovement : MonoBehaviour
 
     void Update()
     {
-        if(m_input.dragMovement)
+        Vector3 pos = transform.position;
+
+
+        if (m_input.dragMovement)
         {
-            //Vector3 newPos = new Vector3( m_prevPoint.x - m_dragPoint.position.x, m_dragPoint.position.y, m_prevPoint.y - m_dragPoint.position.z );
-            //transform.position = newPos;
             Vector3 pointDiff = m_dragPoint.localPosition - m_prevPoint;
 
             pointDiff.y = 0;
 
             transform.position -= pointDiff * m_sensitivity;
-            //transform.position = m_dragPoint.localPosition;
+        }
 
-            //Debug.Log(m_dragPoint.localPosition);
-            //Debug.Log(m_dragPoint.position);
-
-            //Debug.Log(pointDiff * 1000);
+        //keeps player within world bounds
+        if (transform.position.x > m_maxBounds.x)
+        {
+            transform.position = new Vector3(m_prevPos.x, pos.y, pos.z);
+        }
+        if (transform.position.x < m_minBounds.x)
+        {
+            transform.position = new Vector3(m_prevPos.x, pos.y, pos.z);
+        }
+        if (transform.position.z > m_maxBounds.y)
+        {
+            transform.position = new Vector3(pos.x, pos.y, m_prevPos.z);
+        }
+        if (transform.position.z < m_minBounds.y)
+        {
+            transform.position = new Vector3(pos.x, pos.y, m_prevPos.z);
         }
 
         m_prevPoint = m_dragPoint.localPosition;
+        m_prevPos = transform.position;
     }
 }
