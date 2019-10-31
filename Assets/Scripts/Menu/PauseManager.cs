3ac9a1789d039a157b952dcebf762ac2cae3e236
maxbrogno <mgbrogno@gmail.com>
2019-10-31 20:32:46 +1100

    Brought menu scripts into the 21st century

--- Assets/Scripts/Menu/PauseManager.cs
+++ Assets/Scripts/Menu/PauseManager.cs
@@ -1,51 +1,92 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour {
 
+    // UI Panels
     public GameObject pausePanel;
+    public GameObject creditsPanel;
 
+    // Main Buttons
     public Button resumeButton;
-    public Button menuButton;
+    public Button restartButton;
+    public Button creditsButton;
+    public Button exitButton;
 
-    bool isPaused;
+    // Back Buttons
+    public Button creditsBackButton;
+
+    // View State
+    CurrentView currentView;
+    public enum CurrentView {
+        GameplayView,
+        PauseView,
+        CreditsView
+    }
 
     void Start() {
+        // Set the view to the main menu
+        SetView(CurrentView.GameplayView);
+
+        // Assign buttons
         resumeButton.GetComponent<Button>().onClick.AddListener(PressResume);
-        menuButton.GetComponent<Button>().onClick.AddListener(PressMenu);
-        pausePanel.SetActive(false);
+        restartButton.GetComponent<Button>().onClick.AddListener(PressRestart);
+        creditsButton.GetComponent<Button>().onClick.AddListener(PressCredits);
+        exitButton.GetComponent<Button>().onClick.AddListener(PressExit);
+        creditsBackButton.GetComponent<Button>().onClick.AddListener(PressBack);
+
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            isPaused = !isPaused;
-            if (isPaused) Pause();
-            else Unpause();
+        if (Input.GetKeyDown(KeyCode.Escape)) PressPause();
+    }
+
+    void SetView(CurrentView requestedView) {
+        currentView = requestedView;
+
+        // Show and hide panels depending on view
+        switch (currentView) {
+            case CurrentView.GameplayView:
+                Time.timeScale = 1.0f;
+                pausePanel.SetActive(false);
+                creditsPanel.SetActive(false);
+                break;
+
+            case CurrentView.PauseView:
+                Time.timeScale = 0.0f;
+                pausePanel.SetActive(true);
+                creditsPanel.SetActive(false);
+                break;
+
+            case CurrentView.CreditsView:
+                pausePanel.SetActive(false);
+                creditsPanel.SetActive(true);
+                break;
         }
     }
 
+    void PressPause() {
+        SetView(CurrentView.PauseView);
+    }
+
     void PressResume() {
-        Unpause();
-        pausePanel.SetActive(false);
+        SetView(CurrentView.GameplayView);
     }
 
-    void PressMenu() {
-        Unpause();
-        SceneManager.LoadScene(0);
+    void PressRestart() {
+        
     }
 
-    void Pause() {
-        isPaused = true;
-        Time.timeScale = 0.0f;
-        pausePanel.SetActive(true);
+    void PressCredits() {
+        SetView(CurrentView.CreditsView);
     }
 
-    void Unpause() {
-        isPaused = false;
-        Time.timeScale = 1.0f;
-        pausePanel.SetActive(false);
+    void PressExit() {
+        Application.Quit();
+    }
+
+    void PressBack() {
+        SetView(CurrentView.GameplayView);
     }
 }
